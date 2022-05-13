ac3335c8a42405a6e36776b7eb816700914863ad
aidar <aidarkhalilov4021@mail.ru>
2022-05-13 22:08:47 +0300

    Initial commit

---
+++ src/test/java/com/example/fireside/FiresideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fireside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiresideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
