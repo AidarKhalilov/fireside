f9da875b625a14d2dc521ed88a1790583d9b538e
aidar <aidarkhalilov4021@mail.ru>
2022-05-16 01:30:26 +0300

    Recipe Entity

---
+++ src/main/java/com/example/fireside/entity/Recipe.java
@@ -0,0 +1,36 @@
+package com.example.fireside.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String category;
+
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private User author;
+
+    public Recipe() {}
+
+    public Recipe(String title, String description, String category, String image, User author) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.image = image;
+        this.author = author;
+    }
+
+}
