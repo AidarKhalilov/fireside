6f2334841669f12222ae6c73e03a9eff91ad8769
aidar <aidarkhalilov4021@mail.ru>
2022-05-16 23:49:37 +0300

    Swagger added

--- src/main/java/com/example/fireside/FiresideApplication.java
+++ src/main/java/com/example/fireside/FiresideApplication.java
@@ -1,8 +1,20 @@
 package com.example.fireside;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class FiresideApplication {
 
@@ -10,4 +22,26 @@ public class FiresideApplication {
 		SpringApplication.run(FiresideApplication.class, args);
 	}
 
+	private ApiInfo getApiInfo() {
+		return new ApiInfo("recipes", "sample desc", "1.0", "", "some@mail", "MIT", "");
+	}
+
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName()))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(getApiInfo());
+	}
+}
+
+@Controller
+@ApiIgnore
+class SwaggerController {
+	@RequestMapping("/")
+	public String index() {
+		return "redirect:swagger-ui.html";
+	}
 }
